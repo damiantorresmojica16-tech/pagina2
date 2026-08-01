11eb15faa4fb7b87d0a85ed400fab840d31b2f8e
damiantorresmojica16-tech <damiantorresmojica16@gmail.com>
2026-08-01 05:38:17 +0200

    Update bot_integration.py

--- bot_integration.py
+++ bot_integration.py
@@ -1,6 +1,6 @@
 import requests
 
-API_URL = "https://crack-vqey.onrender.com"  # Cambia esto por tu URL de Render
+API_URL = "https://pagina2-2.onrender.com"  # Cambia esto por tu URL de Render
 BOT_SECRET = "8712217253:AAGmnCdbIYFLOKtaHQLjLoGlc7bWsDgo2xI"  # Cambia esto por tu BOT_SECRET configurado
 
 def register_key(key: str, duration_days: int):
